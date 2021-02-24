2b77a7a67b7a31c102bf9443bca99d738895a01f
Atul Kale <kaleatul9@gmail.com>
2021-02-24 20:26:53 +0530

    Hamburger and one js function added.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
 var nav_element = document.querySelectorAll('.nav_element'); // Here we select all navigation elements.
 var images_figure_div = document.getElementsByClassName('figure_div'); // Here we select all images.
+var hamburger = document.querySelector('.hamburger');
+var clickrecord = 0; // Set 1 if hamburger get clicked or set to 0.
+
+hamburger.addEventListener('click', toggle_menu); // This function toggle navigation menu in responsive mode.
 
 nav_element.forEach(function (item, index) {
   // Add event listener to all elements.
-  item.addEventListener('click', filter_and_show_img);
+  item.addEventListener('click', filter_and_show_img); // This function filter and show images that belogs to specific category.
+  item.addEventListener('click', active_button); // This function gives styles to active category and remove styles of unactive category.
 });
 
 function filter_and_show_img(e) {
-  // This function filter and show images that belogs to specific category.
   var category_name = e.target.innerText; // Create category_name variable to store specific category of each element.
 
   switch (
@@ -101,9 +105,15 @@ function filter_and_show_img(e) {
 function show_figure_div(element) {
   // This function make element show itself.
   element.style.cssText = 'display: block;';
+  var curr_window_width = window.innerWidth; // Storing window width to give style for responsive.
   setTimeout(function () {
-    element.children[0].style.cssText =
-      'transition: .5s; width: 260px; height: 200px;';
+    if (curr_window_width > 1080) {
+      element.children[0].style.cssText =
+        'transition: .5s; width: 260px; height: 200px;';
+    } else {
+      element.children[0].style.cssText =
+        'transition: .5s; width: 120px; height: 85px;';
+    }
   }, 500);
 }
 
@@ -113,4 +123,32 @@ function hide_figure_div(element) {
   setTimeout(function () {
     element.style.cssText = 'display: none;';
   }, 500);
+}
+
+function active_button(e) {
+  var each_navigation_element = e.target;
+  each_navigation_element.classList.add('active_btn');
+
+  var node = each_navigation_element.parentNode.firstChild; // Store firstChild for start of looping.
+
+  while (node) {
+    if (node !== each_navigation_element && node.nodeType === Node.ELEMENT_NODE)
+      // Remove styles of unactive elements.
+      node.classList.remove('active_btn');
+    node = node.nextElementSibling;
+  }
+}
+
+// Hamburger code starts here.
+function toggle_menu(e) {
+  var nav_menu = hamburger.nextElementSibling;
+  if (clickrecord === 0) {
+    nav_menu.classList.remove('menu_hide');
+    nav_menu.classList.add('menu_show');
+    clickrecord = 1;
+  } else {
+    nav_menu.classList.remove('menu_show');
+    nav_menu.classList.add('menu_hide');
+    clickrecord = 0;
+  }
 }
